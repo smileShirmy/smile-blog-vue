e79e3b29bd0d580e6932d63de70e8362ce064241
邱诗铭 <qiushiming1997@gmail.com>
2019-05-16 22:52:39 +0800

    [add] 添加移动端菜单栏组件

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="container">
+  <header class="container" :class="homeMobileClass">
     <div class="header-logo">
       <i class="icon" :class="isLight ? 'icon-dark' : 'icon-light'" @click="switchTheme"></i>
       <i class="logo">smile</i>
@@ -7,19 +7,26 @@
     <nav class="nav-wrapper">
       <ul class="nav-list">
         <li class="nav-list-item" v-for="(item, index) in navList" :key="index">
-          <router-link active-class="nav-active" class="nav-link" tag="a" :to="item.link">{{item.name}}</router-link>
+          <router-link
+            active-class="nav-active"
+            class="nav-link"
+            tag="a"
+            :to="item.link"
+          >{{item.name}}</router-link>
           <span class="nav-dot"></span>
         </li>
-        <li class="nav-list-item nav-search"></li>
+        <li class="nav-list-item">
+          <i class="icon icon-search"></i>
+        </li>
       </ul>
-      <div class="mobile-nav-btn" :class="{'is-active': isShowMobileNav}" @click="isShowMobileNav = !isShowMobileNav">
-        <span class="mobile-nav-inner"></span>
-      </div>
+      <mobile-nav :navList="navList" :homeMobileClass="homeMobileClass"></mobile-nav>
     </nav>
   </header>
 </template>
 
 <script>
+import MobileNav from "./mobile-nav";
+
 const navList = [
   {
     link: "home",
@@ -40,25 +47,34 @@ const navList = [
 ];
 
 export default {
+  components: {
+    MobileNav
+  },
+
   data() {
     return {
       navList,
-      isShowMobileNav: false,
       isLight: true
     };
   },
 
+  computed: {
+    homeMobileClass() {
+      return this.$route.path.includes("home") ? "home-mobile-class" : "";
+    }
+  },
+
   methods: {
     switchTheme() {
-      this.isLight = !this.isLight
+      this.isLight = !this.isLight;
 
-      const theme = this.isLight ? 'light' : 'dark'
-      document.body.id = theme
+      const theme = this.isLight ? "light" : "dark";
+      document.body.id = theme;
     }
   },
 
   created() {
-    document.body.id = 'light'
+    document.body.id = "light";
   }
 };
 </script>
@@ -81,10 +97,6 @@ export default {
     padding: 0 85px;
   }
 
-  @media (max-width: 1023px) {
-    position: fixed;
-  }
-
   @media (max-width: 479px) {
     height: 100px;
     padding: 0 35px;
@@ -96,14 +108,12 @@ export default {
     margin-right: 10px;
     font-size: $--title-font-size-medium;
     cursor: pointer;
+    transition: $--theme-transition;
   }
 
   .logo {
     font-size: $--title-font-size-medium;
-  }
-
-  @media (max-width: 1024px) {
-    color: #fff;
+    transition: $--theme-transition;
   }
 }
 
@@ -122,9 +132,10 @@ export default {
 
     .nav-link {
       cursor: pointer;
+      transition: $--theme-transition;
 
       &:hover ~ .nav-dot {
-        background-color: #d3d5dc;
+        background-color: $--nav-dot-color;
       }
     }
 
@@ -147,99 +158,15 @@ export default {
       border-radius: 50%;
       transition: all .25s ease;
     }
-  }
 
-  .nav-search {
-    box-sizing: border-box;
-    position: relative;
-    width: 18px;
-    height: 18px;
-    border: 4px solid var(--font-color-primary);
-    border-radius: 50%;
-    transition: all .25s ease;
-    cursor: pointer;
+    .icon-search {
+      font-size: $--font-size-large;
+      cursor: pointer;
+      transition: $--theme-transition;
 
-    &::after {
-      content: "";
-      position: absolute;
-      width: 8px;
-      height: 4px;
-      background: var(--font-color-primary);
-      top: 110%;
-      right: -8px;
-      transform: rotate(45deg);
-      transition: all .25s ease;
-    }
-
-    &:hover {
-      border-color: var(--nav-active);
-
-      &::after {
-        background: var(--nav-active);
+      &:hover {
+        color: var(--nav-active);
       }
-    }
-  }
-}
-
-.mobile-nav-btn {
-  position: relative;
-  display: none;
-  width: 30px;
-  height: 24px;
-  cursor: pointer;
-
-  .mobile-nav-inner {
-    position: absolute;
-    top: 10px;
-    width: 30px;
-    height: 4px;
-    background-color: var(--font-color-primary);
-    transition: bottom .08s 0s ease-out, top .08s 0s ease-out, opacity 0s linear;
-
-    &::before {
-      content: "";
-      display: block;
-      position: absolute;
-      top: -10px;
-      width: 30px;
-      height: 4px;
-      background-color: var(--font-color-primary);
-      transition: bottom .08s 0s ease-out, top .08s 0s ease-out, opacity 0s linear;
-    }
-
-    &::after {
-      content: "";
-      display: block;
-      position: absolute;
-      top: 10px;
-      width: 30px;
-      height: 4px;
-      background-color: var(--font-color-primary);
-      transition: bottom .08s 0s ease-out, top .08s 0s ease-out, opacity 0s linear;
-    }
-  }
-
-  @media (max-width: 1023px) {
-    .mobile-nav-inner {
-      background-color: #fff;
-      
-      &::before,
-      &::after {
-        background-color: #fff;
-      }
-    }
-  }
-}
-
-.is-active {
-  .mobile-nav-inner {
-
-    &::before {
-      top: 0;
-    }
-
-    &::after {
-      top: 0;
     }
   }
 }
@@ -248,9 +175,15 @@ export default {
   .nav-list {
     display: none;
   }
+}
 
-  .mobile-nav-btn {
-    display: inline-block;
+@media (max-width: 1023px) {
+  .home-mobile-class {
+    position: fixed;
+
+    .header-logo {
+      color: #fff;
+    }
   }
 }
 </style>
---
+++ src/components/header/mobile-nav.vue
@@ -0,0 +1,215 @@
+<template>
+  <nav class="mobile-nav-container">
+    <div
+      class="mobile-nav-btn"
+      :class="{'is-active': isShowMobileNav, homeMobileClass}"
+      @click="isShowMobileNav = !isShowMobileNav"
+    >
+      <span class="mobile-nav-inner"></span>
+    </div>
+    <transition mame="fade">
+      <ul class="nav-list" v-if="isShowMobileNav">
+        <li class="nav-list-item" v-for="(item, index) in navList" :key="index">
+          <router-link
+            active-class="nav-active"
+            class="nav-link"
+            tag="a"
+            :to="item.link"
+          >{{item.name}}</router-link>
+          <span class="nav-dot"></span>
+        </li>
+        <li class="nav-list-item">
+          <i class="icon icon-search"></i>
+        </li>
+      </ul>
+    </transition>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    navList: Array,
+    homeMobileClass: String
+  },
+
+  data() {
+    return {
+      isShowMobileNav: false
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/common/scss/variable.scss";
+@import "@/common/scss/mixin.scss";
+
+.mobile-nav-container {
+  position: relative;
+  display: none;
+
+  @media (max-width: 1023px) {
+    display: inline-block;
+  }
+}
+
+.mobile-nav-btn {
+  @include extend-click;
+  position: relative;
+  width: 30px;
+  height: 24px;
+  cursor: pointer;
+
+  .mobile-nav-inner {
+    position: absolute;
+    top: 10px;
+    width: 30px;
+    height: 4px;
+    background-color: var(--font-color-primary);
+    transition: bottom 0.08s 0s ease-out, top 0.08s 0s ease-out,
+      opacity 0s linear, color .1s linear;
+
+    &::before {
+      content: "";
+      display: block;
+      position: absolute;
+      top: -10px;
+      width: 30px;
+      height: 4px;
+      background-color: var(--font-color-primary);
+      transition: bottom 0.08s 0s ease-out, top 0.08s 0s ease-out,
+        opacity 0s linear, color .1s linear;
+    }
+
+    &::after {
+      content: "";
+      display: block;
+      position: absolute;
+      top: 10px;
+      width: 30px;
+      height: 4px;
+      background-color: var(--font-color-primary);
+      transition: bottom 0.08s 0s ease-out, top 0.08s 0s ease-out,
+        opacity 0s linear;
+    }
+  }
+
+  @media (max-width: 1023px) {
+    .mobile-nav-inner {
+      background-color: var(--font-color-primary);
+
+      &::before,
+      &::after {
+        background-color: var(--font-color-primary);
+      }
+    }
+  }
+}
+
+.is-active {
+  .mobile-nav-inner {
+    &::before {
+      top: 0;
+    }
+
+    &::after {
+      top: 0;
+    }
+  }
+}
+
+.nav-list {
+  box-sizing: border-box;
+  position: absolute;
+  top: 40px;
+  right: -10px;
+  width: 160px;
+  padding: 20px 28px 20px 0;
+  border-radius: 4px;
+  background: rgba(0, 0, 0, .1);
+
+  .nav-list-item {
+    position: relative;
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 10px;
+    font-weight: $--font-weight-bold;
+
+    &:first-child {
+      margin-top: 0;
+    }
+
+    a {
+      cursor: pointer;
+    }
+
+    .nav-link {
+      cursor: pointer;
+
+      &:hover ~ .nav-dot {
+        background-color: $--nav-dot-color;
+      }
+    }
+
+    .nav-active {
+      & ~ .nav-dot {
+        background-color: var(--nav-active);
+      }
+
+      &:hover ~ .nav-dot {
+        background-color: var(--nav-active);
+      }
+    }
+
+    .nav-dot {
+      position: absolute;
+      top: -4px;
+      right: -10px;
+      width: 7px;
+      height: 7px;
+      border-radius: 50%;
+      transition: all 0.25s ease;
+    }
+
+    .icon-search {
+      font-size: $--font-size-large;
+      cursor: pointer;
+      transition: $--theme-transition;
+
+      &:hover {
+        color: var(--nav-active);
+      }
+    }
+  }
+}
+
+@media (max-width: 1023px) {
+  .home-mobile-class {
+    .mobile-nav-inner {
+      background-color: #fff;
+
+      &::before,
+      &::after {
+        background-color: #fff;
+      }
+    }
+
+    .nav-list {
+      color: #fff;
+
+      .nav-link {
+        color: #fff;
+        transition: $--theme-transition;
+      }
+    }
+  }
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1s ease;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
