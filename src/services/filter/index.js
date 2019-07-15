288bfa5d0b5a4af3d072e17e234887e38a3dc6fd
smileShirmy <qiushiming1997@gmail.com>
2019-07-15 15:23:44 +0800

    update: 优化过滤器

--- src/services/filter/index.js
+++ src/services/filter/index.js
@@ -2,6 +2,9 @@ import Utils from '../utils/util'
 
 export default {
   filterTime(date, formats) {
+    if (!date) {
+      return ''
+    }
     return Utils.timestampToTime(date, formats)
   },
 }
